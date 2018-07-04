ef4015f46ae893627ee89a09c162a783d0e50631
sanu23kumar <sanu23kumar@gmail.com>
2018-07-04 19:24:40 +0530

    added string

--- app/src/main/java/com/example/sanukumar/billsplit/GoogleSignIn.java
+++ app/src/main/java/com/example/sanukumar/billsplit/GoogleSignIn.java
@@ -5,4 +5,7 @@ package com.example.sanukumar.billsplit;
  */
 
 public class GoogleSignIn {
+
+
+
 }
--- app/src/main/java/com/example/sanukumar/billsplit/MainActivity.java
+++ app/src/main/java/com/example/sanukumar/billsplit/MainActivity.java
@@ -15,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         int j;
 
+        String s2 = "new";
+
     }
 }
